f35c7e58f3fc650e6258e2fc817fb6a8eb121b5f
gaoyong <gaoyong06@qq.com>
2017-12-04 19:42:24 +0800

    修改：测试

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 //app.js
 var constant = require('constant.js')
 App({
-  onLaunch: function () {
+
+  onLaunch: function (options) {
+    // Do something initial when launch.
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+
   },
-  getUserInfo:function(cb){
+  onShow: function (options) {
+    // Do something when show.
+    console.log("App onShow run");
+  },
+  onHide: function () {
+    // Do something when hide.
+    console.log("App onHide run");
+  },
+  onError: function (msg) {
+    console.log("App onHide run");
+    console.log(msg);
+  },
+  
+  globalData: 'I am global data',
+
+  getUserInfo: function (cb) {
     var that = this;
-    if(this.globalData.userInfo){
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
       wx.login({
         success: function () {
@@ -27,13 +45,9 @@ App({
   },
 
   // TODO:constant啰嗦了
-  globalData:{
+  globalData: {
     // userInfo:null,
-    
+
   },
-  constant:constant.constant,
-  
+  constant: constant.constant,
 })
-
-
-
